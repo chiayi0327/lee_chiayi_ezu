23846d3d36f74a92ba32b52a11f5ba4b982a2cbe
chiayi0327 <30381945+chiayi0327@users.noreply.github.com>
2022-04-08 16:21:25 -0500

    fix instructor and student list

--- lee_chiayi_ezu/courseinfo/views.py
+++ lee_chiayi_ezu/courseinfo/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.views.generic import ListView
 
 from courseinfo.forms import InstructorForm, SectionForm, CourseForm, SemesterForm, StudentForm, RegistrationForm
 from courseinfo.models import (
@@ -11,52 +12,12 @@ from courseinfo.models import (
 	Student,
 	Registration,
 )
-from courseinfo.utils import ObjectCreateMixin
+from courseinfo.utils import ObjectCreateMixin, PageLinksMixin
 
 
-class InstructorList(View):
-	page_kwarg = 'page'
-	paginate_by = 25;  # 25 instructors per page
-	template_name = 'courseinfo/instructor_list.html'
-
-	def get(self, request):
-		instructors = Instructor.objects.all()
-		paginator = Paginator(
-			instructors,
-			self.paginate_by
-		)
-		page_number = request.GET.get(
-			self.page_kwarg
-		)
-		try:
-			page = paginator.page(page_number)
-		except PageNotAnInteger:
-			page = paginator.page(1)
-		except EmptyPage:
-			page = paginator.page(
-				paginator.num_pages)
-		if page.has_previous():
-			prev_url = "?{pkw}={n}".format(
-				pkw=self.page_kwarg,
-				n=page.previous_page_number())
-		else:
-			prev_url = None
-		if page.has_next():
-			next_url = "?{pkw}={n}".format(
-				pkw=self.page_kwarg,
-				n=page.next_page_number())
-		else:
-			next_url = None
-		context = {
-			'is_paginated':
-				page.has_other_pages(),
-			'next_page_url': next_url,
-			'paginator': paginator,
-			'previous_page_url': prev_url,
-			'instructor_list': page,
-		}
-		return render(
-			request, self.template_name, context)
+class InstructorList(PageLinksMixin, ListView):
+	paginate_by = 25
+	model = Instructor
 
 
 class InstructorDetail(View):
@@ -501,7 +462,7 @@ class StudentList(View):
 			'next_page_url': next_url,
 			'paginator': paginator,
 			'previous_page_url': prev_url,
-			'instructor_list': page,
+			'student_list': page,
 		}
 		return render(
 			request, self.template_name, context)
