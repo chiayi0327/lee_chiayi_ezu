f39d81d3d6b765a86a5259fc8db9e681878234ef
chiayi0327 <30381945+chiayi0327@users.noreply.github.com>
2022-04-15 16:19:56 -0500

    authentication for app

--- lee_chiayi_ezu/lee_chiayi_ezu/settings.py
+++ lee_chiayi_ezu/lee_chiayi_ezu/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -124,6 +126,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
 LOGOUT_REDIRECT_URL = 'login_urlpattern'
+
+LOGIN_URL = reverse_lazy('login_urlpattern')
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
