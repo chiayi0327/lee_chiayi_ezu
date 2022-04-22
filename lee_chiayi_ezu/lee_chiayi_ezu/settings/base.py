48075ceef9b74a51ab26352ad777d462d4924ab8
chiayi0327 <30381945+chiayi0327@users.noreply.github.com>
2022-04-22 00:25:59 -0500

    backup commit
    backup commit

--- lee_chiayi_ezu/lee_chiayi_ezu/settings/base.py
+++ lee_chiayi_ezu/lee_chiayi_ezu/settings/base.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'lee_chiayi_ezu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
